7ed1b8c8e5a7b14157561e57516c6a972e2dcd91
Raphael Mello <raphaelpmros@hotmail.com>
2024-03-19 14:35:16 -0300

    começando o CRUD de user, funções allUsers e CreateUser prontas

--- backEnd/controllers/users.js
+++ backEnd/controllers/users.js
@@ -1,3 +1,6 @@
 const con = require("../database/db");
 const bcrypt = require("bcrypt");
 const salts = 10;
+const User = require("../models/User");
+
+models
--- backEnd/models/User.js
+++ backEnd/models/User.js
@@ -1,12 +1,12 @@
-const con = require('../database/db');
+const con = require("../database/db");
 
 con.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar-se ao banco de dados:', err.stack);
+    console.error("Erro ao conectar-se ao banco de dados:", err.stack);
     return;
   }
 
-  console.log('Conectado ao banco de dados.');
+  console.log("Conectado ao banco de dados.");
 });
 
 const createLibraryTableSQL = `
@@ -17,14 +17,61 @@ CREATE TABLE users(
     cpf VARCHAR(14),
     full_address VARCHAR(45),
     additional_address_details VARCHAR(45),
-    phone VARCHAR(14)
+    phone VARCHAR(14),
+    password VARCHAR(30)
 )`;
 
 con.query(createLibraryTableSQL, (err, result) => {
   if (err) {
-    console.error('Erro ao criar tabela users:', err);
+    console.error("Erro ao criar tabela users:", err);
     return;
   }
 
-  console.log('Tabela users criada com sucesso.');
+  console.log("Tabela users criada com sucesso.");
 });
+
+function allUsers() {
+  return new Promise((resolve, reject) => {
+    const sql = "SELECT * FROM users";
+    con.query(sql, (err, results) => {
+      if (err) {
+        console.error(err);
+        reject(err);
+      }
+      resolve(results);
+    });
+  });
+}
+
+function createUser(
+  full_name,
+  email,
+  cpf,
+  full_address,
+  additional_address_details,
+  phone,
+  password
+) {
+  return new Promise((resolve, reject) => {
+    const sql =
+      "INSERT INTO users (full_name, email, cpf, full_address, additional_address_details, phone, password) (?, ?, ?, ?, ?, ?, ?)";
+    const values = [
+      full_name,
+      email,
+      cpf,
+      full_address,
+      additional_address_details,
+      phone,
+      password,
+    ];
+    con.query(sql, values, (err, result) => {
+      if (err) {
+        console.error(err);
+        return reject(err);
+      }
+      resolve(result);
+    });
+  });
+}
+
+
