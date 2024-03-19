05ea9ab5d7323b69c0919b7d934a00a381cbbec2
Raphael Mello <raphaelpmros@hotmail.com>
2024-03-19 16:21:41 -0300

    arrumando erro de escrita no código

--- backEnd/controllers/users.js
+++ backEnd/controllers/users.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 const salts = 10;
 const User = require("../models/User");
 
-models.export.users = async (req, res) => {
+module.export.users = async (req, res) => {
   try {
     const viewUsers = await User.allUsers();
     return res.status(200).json({ viewUsers });
