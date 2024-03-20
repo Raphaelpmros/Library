52150057a57aad3cede83fa4596c60a74397c9fc
Raphael Mello <raphaelpmros@hotmail.com>
2024-03-20 16:04:59 -0300

    lógica viewBooks feita

--- backEnd/controllers/books.js
+++ backEnd/controllers/books.js
@@ -1,1 +1,12 @@
-const Book = require("../models/book");
+const Book = require("../models/Book");
+
+module.exports.books = async (req, res) => {
+    try{
+        const viewBooks = await Book.allBooks();
+        return res.status(200).json({viewBooks});
+    } catch (err) {
+        console.error(err)
+        return res.status(500).json({message: "Erro interno do servidor"})
+    }
+}
+
