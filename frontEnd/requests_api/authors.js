64fae87f35f21be270080f83bb1a7c18ec696d5f
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-03 14:44:33 -0300

    rotas de authors configurada (sem teste ainda)

--- frontEnd/requests_api/authors.js
+++ frontEnd/requests_api/authors.js
@@ -1,10 +1,37 @@
 import fetchApi from "../axios/api";
 
-export const authors = async () => {
+export const viewAuthors = async () => {
   try {
-    const response = await fetchApi.get("/authors");
+    const response = await fetchApi.get(`/authors`);
     return response.data;
   } catch (error) {
     throw error;
   }
 };
+
+export const newAuthor = async (authors, config) => {
+  try {
+    const response = await fetchApi.post(`/authors/new`, authors, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateAuthor = async (id, authors, config) => {
+  try {
+    const response = await fetchApi.post(`/authors/update`, id, authors, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteAuthor = async (id, config) => {
+    try {
+        const response = await fetchApi.delete(`/authors/${id}`, config);
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+}
