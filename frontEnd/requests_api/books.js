eb2962a0bf83a70c725f5b88b40e24b68896d55e
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-03 15:39:59 -0300

    rota dos books feito na chamada da API

--- frontEnd/requests_api/books.js
+++ frontEnd/requests_api/books.js
@@ -1,1 +1,37 @@
 import fetchApi from "../axios/api";
+
+export const viewBooks = async () => {
+  try {
+    response = await fetchApi.get(`/books`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const newBook = async (books, config) => {
+  try {
+    response = await fetchApi.post(`/books`, books, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateBook = async (id, books, config) => {
+    try {
+        response = await fetchApi.post(`/books/update`, id, books, config);
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+}
+
+export const deleteBooks = async (id, config) => {
+    try {
+        response = await fetchApi.delete(`/books/${id}`, config);
+        return response.data;
+      } catch (error) {
+        throw error;
+      }
+}
