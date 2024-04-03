c2ed070cde48d2f6ad182e1669ee763653d0fe71
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-03 15:59:16 -0300

    rota api reviews feita

--- frontEnd/requests_api/reviews.js
+++ frontEnd/requests_api/reviews.js
@@ -1,1 +1,28 @@
 import fetchApi from "../axios/api";
+
+export const allReviews = async () => {
+  try {
+    response = await fetchApi.get(`/reviews`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const newReviews = async (reviews, config) => {
+  try {
+    response = await fetchApi.post(`/reviews`, reviews, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteReviews = async (id) => {
+  try {
+    response = await fetchApi.delete(`/reviews/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
