16768475dfe0787cf46b48bed95214ecef377029
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-03 15:56:59 -0300

    rota api rents feita

--- frontEnd/requests_api/categories.js
+++ frontEnd/requests_api/categories.js
@@ -1,37 +1,37 @@
 import fetchApi from "../axios/api";
 
 export const allCategories = async () => {
-    try {
-        response = await fetchApi.get(`/categories`);
-        return response.data;
-      } catch (error) {
-        throw error;
-      }
-}
+  try {
+    response = await fetchApi.get(`/categories`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const newCategories = async (categories, config) => {
-    try {
-        response = await fetchApi.post(`/categories`, categories, config);
-        return response.data;
-      } catch (error) {
-        throw error;
-      }
-}
+  try {
+    response = await fetchApi.post(`/categories`, categories, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const updateCategories = async (id, categories, config) => {
-    try {
-        response = await fetchApi.post(`/categories`, id, categories, config);
-        return response.data;
-      } catch (error) {
-        throw error;
-      }
-}
+  try {
+    response = await fetchApi.post(`/categories`, id, categories, config);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
 
 export const deleteCategories = async (id) => {
-    try {
-        response = await fetchApi.delete(`/categories/${id}`);
-        return response.data;
-      } catch (error) {
-        throw error;
-      }
-}
+  try {
+    response = await fetchApi.delete(`/categories/${id}`);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
