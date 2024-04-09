29ddeb550c942b61bd8d78a6b714bfed34a03042
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-09 17:31:34 -0300

    importando as rotas no app.jsx

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,17 +1,9 @@
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import React, { useEffect } from "react";
+import { Routes, Route, Router, useLocation } from "react-router-dom";
 
-import NewAuthors from "./pages/authors/newAuthors";
-import AllAuthors from "./pages/authors/allAuthors";
-import EditAuthors from "./pages/authors/editAuthors";
-
-import NewBooks from "./pages/books/newBooks";
-import AllBooks from "./pages/books/allBooks";
-import EditBooks from "./pages/books/editBooks";
-
-import EditCategories from "./pages/categories/editCategory";
-import NewCategories from "./pages/categories/newCategories";
-import AllCategories from "./pages/categories/allCategories";
+import Home from "./pages/home/home";
 
 import NewRent from "./pages/rents/newRents";
 import AllRents from "./pages/rents/allRents";
@@ -21,11 +13,57 @@ import EditUser from "./pages/users/editUser";
 import UserPage from "./pages/users/userPage";
 import LoginUser from "./pages/users/loginUser";
 
+import NewBooks from "./pages/books/newBooks";
+import AllBooks from "./pages/books/allBooks";
+import EditBooks from "./pages/books/editBooks";
+
+import NewAuthors from "./pages/authors/newAuthors";
+import AllAuthors from "./pages/authors/allAuthors";
+import EditAuthors from "./pages/authors/editAuthors";
+
+import EditCategories from "./pages/categories/editCategory";
+import NewCategories from "./pages/categories/newCategories";
+import AllCategories from "./pages/categories/allCategories";
+
 function App() {
   return (
     <>
       <Navbar />
-      
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/rents/allrents" element={<AllRents />} />
+        <Route path="/rents/new" element={<NewRent />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/users/new" element={<NewUser />} />
+        <Route path="/users/update" element={<EditUser />} />
+        <Route path="/users" element={<UserPage />} />
+        <Route path="/users/login" element={<LoginUser />} />
+      </Routes>
+
+      <Routes>
+        <Route path="/books/new" element={< NewBooks/>}/>
+        <Route path="/books" element={<AllBooks/>}/>
+        <Route path="/books/update" element={<EditBooks/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/authors/new" element={< NewAuthors/>}/>
+        <Route path="/authors" element={<AllAuthors/>}/>
+        <Route path="/authors/update" element={< EditAuthors/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/categories/update" element={<EditCategories/>}/>
+        <Route path="/categories/new" element={<NewCategories/>}/>
+        <Route path="/categories" element={<AllCategories/>}/>
+      </Routes>
+
       <Footer />
     </>
   );
