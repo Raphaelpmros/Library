778ec43aca8dcc57f97a2e6d6c6c1d08809cd674
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-10 17:08:24 -0300

    arrumando as imagens

--- frontEnd/src/pages/home/home.jsx
+++ frontEnd/src/pages/home/home.jsx
@@ -11,8 +11,8 @@ export default function Home() {
                     <p>In the <span>mystic library</span> no one will charge you to get any book, no one will sell you courses full of empty promisses. You want to be part of our comunity? Click to register or read more about us!</p>
                 </div>
                 <div className="grid grid-cols-2 gap-4 mt-8">
-                    <img className="w-full rounded-lg" src="https://i.pinimg.com/564x/eb/18/0f/eb180f32cae4b20d7743e892f1fc67d5.jpg" alt="office content 1"/>
-                    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://i.pinimg.com/564x/a0/0c/1c/a00c1ce74da914060b26f6bda5855a0d.jpg" alt="office content 2"/>
+                    <img className="w-full rounded-lg" src="https://i.pinimg.com/564x/a0/0c/1c/a00c1ce74da914060b26f6bda5855a0d.jpg" alt="office content 1"/>
+                    <img className="mt-4 w-full lg:mt-10 rounded-lg" src="https://i.pinimg.com/564x/eb/18/0f/eb180f32cae4b20d7743e892f1fc67d5.jpg" alt="office content 2"/>
                 </div>
             </div>
         </section>
