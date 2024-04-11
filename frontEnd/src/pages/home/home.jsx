89351c7b9f2878d84c8592a0ed29ae4e209e5fad
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-11 12:23:17 -0300

    criando link na home para a pagina about e para a pagina register

--- frontEnd/src/pages/home/home.jsx
+++ frontEnd/src/pages/home/home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
                 <div>
                     <h2 className={styles.title}>Welcome to the <span>mystic library</span></h2>
                     <p className="mb-4">This library belong to those how think knowledge shoud be free an acessible to all that wants to find the path of the wise.</p>
-                    <p>In the <span>mystic library</span> no one will charge you to get any book, no one will sell you courses full of empty promisses. You want to be part of our comunity? Click to register or read more about us!</p>
+                    <p>In the <span>mystic library</span> no one will charge you to get any book, no one will sell you courses full of empty promisses. You want to be part of our comunity? <a href="/users/new" className="hover:text-blue-800 italic font-bold">Click to register</a> or read more <a href="/about" className="hover:text-blue-800 italic font-bold">about us</a>!</p>
                 </div>
                 <div className="grid grid-cols-2 gap-4 mt-8">
                     <img className="w-full rounded-lg" src="https://i.pinimg.com/564x/a0/0c/1c/a00c1ce74da914060b26f6bda5855a0d.jpg" alt="office content 1"/>
