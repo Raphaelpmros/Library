c5699c6c4a709c125ab651e6f181dc17f08ada93
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-12 15:57:06 -0300

    arrumando a responsividade

--- frontEnd/src/pages/books/allBooks.jsx
+++ frontEnd/src/pages/books/allBooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import NewBookModal from "./components/NewBookModal";
 import ViewBooksComponents from "./components/ViewBooks";
 import { viewBooks } from "../../../requests_api/books";
@@ -10,7 +10,11 @@ export default function AllBooks() {
     const fetchBooks = async () => {
       try {
         const response = await viewBooks(); // Chama a função viewBooks para obter os dados
-        if (response && response.viewBooks && Array.isArray(response.viewBooks)) {
+        if (
+          response &&
+          response.viewBooks &&
+          Array.isArray(response.viewBooks)
+        ) {
           setBooks(response.viewBooks); // Define os livros no estado após a resposta bem-sucedida
         } else {
           console.error("Error: Invalid data format received");
@@ -30,13 +34,16 @@ export default function AllBooks() {
 
       {/* Renderiza uma mensagem se não houver livros */}
       {books.length === 0 && (
-        <div className="mt-20 text-xl text-center flex justify-center flex-col" style={{ height: "60vh" }}>
+        <div
+          className="mt-20 text-xl text-center flex justify-center flex-col"
+          style={{ height: "60vh" }}
+        >
           <h1 className="text-4xl">We don't have any books yet</h1>
         </div>
       )}
 
       {/* Renderiza a grade de livros se houver livros no estado */}
-      <div className="grid grid-cols-1 md:grid-cols-4 gap-1 p-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-5">
         {books.map((book) => (
           <ViewBooksComponents
             key={book.id}
