2aa15b85208869a3c56ea20bae368dd0cfcf501e
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-15 14:58:03 -0300

    redirecionando o livro para o lugar certo

--- frontEnd/src/pages/books/components/ViewBooksComponent.jsx
+++ frontEnd/src/pages/books/components/ViewBooksComponent.jsx
@@ -35,11 +35,11 @@ export default function ViewBooks({ id, full_name, quantity, image }) {
           >
             {quantity > 0 ? "In Stock" : "Out of Stock"} - Quantity: {quantity}
           </p>
-          <a
-            href="#"
-            className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          <Link
+          to={`/books/${id}`}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
-            Read more
+            About the book
             <svg
               className="rtl:rotate-180 w-3.5 h-3.5 ms-2"
               aria-hidden="true"
@@ -55,7 +55,7 @@ export default function ViewBooks({ id, full_name, quantity, image }) {
                 d="M1 5h12m0 0L9 1m4 4L9 9"
               />
             </svg>
-          </a>
+          </Link>
         </div>
       </div>
     </>
