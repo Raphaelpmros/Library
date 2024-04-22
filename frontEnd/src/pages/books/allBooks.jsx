e9ea14047f01d2885d5709bf9340c5278220fca9
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-22 15:29:07 -0300

    só arrumando o padding

--- frontEnd/src/pages/books/allBooks.jsx
+++ frontEnd/src/pages/books/allBooks.jsx
@@ -54,7 +54,7 @@ export default function AllBooks() {
         </div>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 place-items-center p-5">
         {books.map((book) => (
           <ViewBooksComponents
             key={book.id}
