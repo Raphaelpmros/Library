41628cf84239477da02a2c48b70de1400da0b9b6
Raphael Mello <raphaelpmros@hotmail.com>
2024-04-26 13:41:02 -0300

    passando respostas para o ingles

--- backEnd/database/db.js
+++ backEnd/database/db.js
@@ -12,11 +12,11 @@ const con = mysql.createConnection(conConfig);
 
 con.connect((err) => {
   if (err) {
-    console.error('Erro ao conectar-se ao banco de dados:', err.stack);
+    console.error('Error connecting to database', err.stack);
     return;
   }
 
-  console.log('Conectado ao banco de dados.');
+  console.log('Database connected.');
 });
 
 module.exports = con;
