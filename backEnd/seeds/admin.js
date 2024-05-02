77bba2791bffd52aa11957fdde46fe9575535c54
Raphael Mello <raphaelpmros@hotmail.com>
2024-05-02 15:47:30 -0300

    colocando imagem no admin e arrumando o ultimo erro

--- backEnd/seeds/admin.js
+++ backEnd/seeds/admin.js
@@ -10,7 +10,7 @@ con.connect((err) => {
       const password = "admin123";
       const passwordHash = bcrypt.hashSync(password, salts);
       const checkAdmin = `SELECT * FROM users WHERE email='admin@admin.com'`;
-      const insertAdmin = `INSERT INTO users (full_name, email, cpf, full_address, phone, password, admin) VALUES ('Admin', 'admin@admin.com', '000.000.000-00', 'rua da livraria', '(00)99999-9999', '${passwordHash}', 1);`;
+      const insertAdmin = `INSERT INTO users (full_name, image, email, cpf, full_address, phone, password, admin) VALUES ('Admin', 'https://res.cloudinary.com/dhafchcdg/image/upload/v1714416005/library/cm9fbyfhxl5vtnjso8pn.jpg', 'admin@admin.com', '000.000.000-00', 'rua da livraria', '(00)99999-9999', '${passwordHash}', '1');`;
       con.query(checkAdmin, (err, result) => {
         if (result.length > 0) {
           return console.log("Admin already register");
